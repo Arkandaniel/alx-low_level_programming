e9310ac488e758f7dba316148b5b59ad44c69570
Arkandaniel <21arkandaniel@gmail.com>
2022-06-04 09:28:21 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+	long int l;
+	long long int x;
+	float f;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of a long int is: %lu.\n", (unsigned long)sizeof(l));
+	printf("The size of a long long int is: %lu.\n", (unsigned long)sizeof(x));
+	printf("The size of a float is: %lu.\n", (unsigned long)sizeof(f));
+
+	return (0);
+}
+
+
