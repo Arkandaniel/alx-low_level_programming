0ff49c47569b80c564940cc0bc4f3315a6f4e205
Arkandaniel <21arkandaniel@gmail.com>
2022-06-06 09:37:31 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,11 @@ int main(void)
 	int i;
 	char c;
 
-	for(i = 48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for(c = 'a'; c <= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
