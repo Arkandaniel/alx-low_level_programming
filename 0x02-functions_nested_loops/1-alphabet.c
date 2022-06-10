a8d51f71172d418150df5821d7d31ec6c9ab7ad8
Arkandaniel <21arkandaniel@gmail.com>
2022-06-10 12:56:34 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - alphabets
+ * Print_alphabet - alphabets
  *
- * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -14,5 +13,4 @@ void print_alphabet(void)
 		putchar(c);
 	}
 	putchar('\n');
-	return (0);
 }
