583f011f9a5f44483c9d4e68af897cef199377fa
Arkandaniel <21arkandaniel@gmail.com>
2022-06-13 12:32:58 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps to integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
