621db1fcbe28f83329949c876a5db027666968fd
Arkandaniel <21arkandaniel@gmail.com>
2022-06-23 01:05:14 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-		for ( c = 'a'; c <= 'z'; c++)
+		for ( i = 0; i < 10; i++)
 		{
-			for ( i= 0; i < 10; i++)
+			for ( c = 'a'; c <= 'z'; c++)
 			putchar(c);
 		}
 		putchar('\n');
