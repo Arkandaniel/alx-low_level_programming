323172ba186da62c2353b89ece4b8c91528393d2
Arkandaniel <21arkandaniel@gmail.com>
2022-06-25 03:37:21 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,16 +11,19 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
+
 	       	return (1);
 	}
 	else if (n == 0)
 	{
 		putchar('0');
+
 	       	return (0);
 	}
 	else
 	{
 		putchar('-');
+
 	       	return (-1);
 	}
 }
