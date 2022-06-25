e55bc06b6a72f94be401452868d166ea30a0ccac
Arkandaniel <21arkandaniel@gmail.com>
2022-06-25 06:42:08 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,23 +4,23 @@
 /**
  * print_sign - prints the sign of a number
  * @n: integer parameter
- * return: 1 0 and -1 depending on the value
+ * Return: 1 0 and -1 depending on the value
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (0);
 		putchar('0');
+		return ('0');
 	}
 	else
 	{
-		return (-1);
 		putchar('-');
+		return (-1);
 	}
 }
