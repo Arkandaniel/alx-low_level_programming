fce3f21c572c4cbf2cbec25a242d16869c6046ad
Arkandaniel <21arkandaniel@gmail.com>
2022-06-25 08:26:36 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,10 +3,10 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * 
+ * @n: parameter
  * Return: value
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	return (n % 10);
 }
