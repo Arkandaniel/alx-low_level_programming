ff3b93be24ea1a0e296f64c223ddc441fe063952
Arkandaniel <21arkandaniel@gmail.com>
2022-06-25 10:19:31 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,6 +12,14 @@ void print_to_98(int n)
 	scanf("%d", &n);
 	for (i = n; i <= 98; i++)
 	{
-		printf("%d, ", i);
+		printf("%d", i);
+		if (i < 98)
+			printf(", ");
 	}
+	for (i = n; i >= 98; i++)
+		{
+		printf("%d", i);
+		if (i > 98)
+			printf(", ");
+		}
 }
