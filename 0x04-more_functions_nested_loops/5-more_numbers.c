eb9e3bbe1301ee802466a4e67fe433db55ed44c2
Arkandaniel <21arkandaniel@gmail.com>
2022-06-26 03:20:27 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n <= 14; i++)
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+}
