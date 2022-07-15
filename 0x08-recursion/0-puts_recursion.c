8eeaf0b2892306d89ce50f376ff41e77f512cabd
Arkandaniel <21arkandaniel@gmail.com>
2022-07-15 06:41:04 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
+/**
+ * _puts_recursion - prints a string
+ * @s: param
+ * Return: The string
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	return (_puts_recursion(*s));
 }
