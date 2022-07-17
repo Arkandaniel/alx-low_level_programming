81f31be1b4f57d827c49537e84361941974c75b5
Arkandaniel <21arkandaniel@gmail.com>
2022-07-17 11:02:21 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - exponential function
+ * @x: param 1
+ * @y: param 2
+ * Return: -1,1 or an exponential
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y <= 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (_pow_recursion(x^y));
+}
