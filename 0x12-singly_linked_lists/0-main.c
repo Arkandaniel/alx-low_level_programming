c28b920b4969da2a3c50abb06649ad5e6dbfe180
Arkandaniel <21arkandaniel@gmail.com>
2022-07-20 10:58:24 -0700

    Task 0

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -4,20 +4,6 @@
 #include "lists.h"
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
-/**
  * main - check the code
  *
  * Return: Always 0.
