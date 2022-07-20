f4a10922282bb67eb7b460de3c7852020f162a82
Arkandaniel <21arkandaniel@gmail.com>
2022-07-20 07:59:50 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 
 /**
+ * print_list - prints all the list_t element
+ * @h: param
+ * Return: n
  */
 size_t print_list(const list_t *h)
 {
 	int n = 0;
+
 	while (h != NULL)
 	{
 		printf("[%d] %s\n", h->len, h->str);
