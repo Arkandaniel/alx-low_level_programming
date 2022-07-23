af19f001b206f9852f21d779696e22671a01da26
Arkandaniel <21arkandaniel@gmail.com>
2022-07-23 02:33:10 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 
 /**
+ * list_len - prints number of the elements
+ * @h: param
+ * Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
 	int n = 0;
 
-	while (h != NULL)
-		h = h ->next;
-			n++;
-			return (n);
+	struct Node* current = h
+	while (current != NULL)
+	{
+		n++;
+		current = current->next;
+		return (n);
+	}
 }
